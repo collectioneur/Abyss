02638ba8247c253dab15f7de39d172f688c9b488
Yehor <101332730+collectioneur@users.noreply.github.com>
2023-12-30 15:31:14 +0100

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -6,23 +6,24 @@ using namespace std;
 
 Menu::Menu(const Font& font, GameState& gameState) : gameState(gameState) {
     menubuttons.emplace_back("Play", font, 50);
-    menubuttons.emplace_back("Settings", font, 50);
+    //menubuttons.emplace_back("Settings", font, 50);
+    menubuttons.emplace_back("Statistics", font, 50);
     menubuttons.emplace_back("Characters", font, 50);
     menubuttons.emplace_back("End", font, 50);
     playbuttons.emplace_back("Gate\nto\nHell", font, 50);
-    playbuttons.emplace_back("Demonic\nresidences", font, 50);
+   // playbuttons.emplace_back("Demonic\nresidences", font, 50);
     playbuttons.emplace_back("Back", font, 50);
-    float y = 100;
+    float y = 300.f;
     for (auto& button : menubuttons) {
         button.setPosition(500, y);
         y += 80;
     }
     playbuttons[0].setPosition(630, 150);
-    playbuttons[1].setPosition(1000, 300);
-    playbuttons[2].setPosition(650, 680);
+   // playbuttons[1].setPosition(1000, 300);
+    playbuttons[1].setPosition(650, 680);
 }
 
-void Menu::draw(RenderWindow& window) {
+void Menu::draw(RenderWindow& window, Texture& gameLogoTexture) {
     if (showPlayButtons) {
                 for (auto& button : playbuttons) {
                     button.draw(window);
@@ -32,6 +33,7 @@ void Menu::draw(RenderWindow& window) {
             for (auto& button : menubuttons) {
                     button.draw(window);
             }
+        window.draw(createLogo(gameLogoTexture));
         }
 }
 
@@ -59,8 +61,23 @@ void Menu::update(const Vector2f& mousePos, const Event& event) {
             if(button.getTitle() == "End") {
                 exit(0);
             }
+            if(button.getTitle() == "Statistics") {
+                gameState = GameState::STATISTICS;
+            }
+            if(button.getTitle() == "Characters") {
+                gameState = GameState::CHARACTERS;
+            }
         }
     }
     }
-};
+}
+
+Sprite Menu::createLogo(Texture gameLogoTexture) {
+    Sprite gamelogo(gameLogoTexture);
+    float xLogo = 700;
+    float yLogo = 50;
+    gamelogo.setScale(1.5, 1.5);
+    gamelogo.setPosition(xLogo, yLogo);
+    return gamelogo;
+}
 
