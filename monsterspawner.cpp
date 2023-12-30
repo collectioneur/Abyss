991eda00a809fa4d2d3e23032c0ebd31752c9d13
Yehor <101332730+collectioneur@users.noreply.github.com>
2023-12-30 15:32:28 +0100

    Update monsterspawner.cpp

--- monsterspawner.cpp
+++ monsterspawner.cpp
@@ -1,7 +1,7 @@
 #include "monsterspawner.hpp"
-
-MonsterSpawner::MonsterSpawner(const Texture& texture, const FloatRect& area1, const FloatRect& area2)
-    : monsterTexture(texture), spawnArea1(area1), spawnArea2(area2) {}
+#include <iostream>
+MonsterSpawner::MonsterSpawner(const Texture& texture, const FloatRect& area1, const FloatRect& area2, const Texture& bulletTexture)
+    : monsterTexture(texture), spawnArea1(area1), spawnArea2(area2), bulletTexture(bulletTexture) {}
 
 Vector2f MonsterSpawner::getRandomSpawnPosition(const FloatRect& area) {
     random_device rd;
@@ -12,12 +12,13 @@ Vector2f MonsterSpawner::getRandomSpawnPosition(const FloatRect& area) {
     return Vector2f(disX(gen), disY(gen));
 }
 
-void MonsterSpawner::spawnMonster(vector<Monster>& monsters) {
+void MonsterSpawner::spawnMonster(vector<Monster>& monsters,const int monsterType) {
     Vector2f spawnPos;
     if (rand() % 2 == 0) {
         spawnPos = getRandomSpawnPosition(spawnArea1);
     } else {
         spawnPos = getRandomSpawnPosition(spawnArea2);
     }
-    monsters.emplace_back(monsterTexture, spawnPos, 50.0f); 
+   // int monstertype = monsterType;
+    monsters.emplace_back(monsterTexture, spawnPos, monsterType, bulletTexture);
 }
